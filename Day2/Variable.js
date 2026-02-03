b1b12a656def550bc058bcd115fdb91d7a6e6631
lavannya007 <lav2mails@gmail.com>
2026-02-03 11:19:30 +0900

    Initial commit

---
+++ Day2/Variable.js
@@ -0,0 +1,46 @@
+// var data type
+// declare variable, assign 
+// var name - declare
+// name ="kavi" - define
+var name1="lavanya"
+console.log(name1);
+//reassign 
+name1="elango"
+console.log(name1);
+// re declare
+var name1="shan"
+console.log(name1);
+
+// let data type
+let age=30
+console.log(age);
+// reassign 
+age=30
+console.log(age);
+//redeclare
+age=23
+console.log(age);
+//const
+const place="chennai"
+console.log(place);
+//var
+var name1="subi"
+console.log(name1);
+name1="kabi"
+console.log(name1);
+var name1="lavi"
+console.log(name1);
+//let
+let house="Flat"
+console.log(house);
+house="individual"
+console.log(house);
+
+house="old"
+console.log(house);
+//const
+const animals="cat"
+console.log(animals);
+var animal="donkey"
+console.log(animal);
+
